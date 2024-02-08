adfe7a7291a337f3301a515d5184a952613508a8
sandy13oct1992 <sandy13oct1992@gmail.com>
2024-02-08 18:19:05 +0530

    q.no. 5

--- src/component/expenses/expenseForm.js
+++ src/component/expenses/expenseForm.js
@@ -1,18 +1,44 @@
 import React, { useState } from 'react';
 
 const expenseForm=() =>{
-    const [enteredTitle, setenteredTitle] = useState('');
-    const [enteredAmount, setenteredAmount] = useState('');
-    const [enteredDate, setenteredDate] = useState('');
+    // const [enteredTitle, setenteredTitle] = useState('');
+    // const [enteredAmount, setenteredAmount] = useState('');
+    // const [enteredDate, setenteredDate] = useState('');
 
+    const [userInput, setUserInput] = useState({
+        enteredTitle:'',
+        enteredAmount:'',
+        enteredDate:'',
+    });
 const titleChangeHandler = (event) => {
-    setenteredTitle(event.target.value);
+    // setenteredTitle(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredTitle: event.target.value,
+    // })
+    setUserInput((prevState) =>{
+        return {...prevState, enteredTitle:event.target.value};
+    });
 }
     const amountChangeHandler = (event) => {
-        setenteredAmount(event.target.value);
+        // setenteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // }) 
+        setUserInput((prevState) =>{
+            return {...prevState, enteredAmount:event.target.value};
+        });
     }
     const dateChangeHandler = (event) => {
-       setenteredDate(event.target.value);
+    //    setenteredDate(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredDate: event.target.value,
+    // }) 
+    setUserInput((prevState) =>{
+        return {...prevState, enteredDate:event.target.value};
+    });
 }
 return <form>
     <div>
