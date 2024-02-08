46983cdad0a59339a8522e2bd265b98c9274c6ab
sandy13oct1992 <sandy13oct1992@gmail.com>
2024-02-08 17:04:47 +0530

    q.no-4

--- src/component/expenses/expenseForm.js
+++ src/component/expenses/expenseForm.js
@@ -1,22 +1,32 @@
-
+import React, { useState } from 'react';
 
 const expenseForm=() =>{
+    const [enteredTitle, setenteredTitle] = useState('');
+    const [enteredAmount, setenteredAmount] = useState('');
+    const [enteredDate, setenteredDate] = useState('');
+
 const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setenteredTitle(event.target.value);
+}
+    const amountChangeHandler = (event) => {
+        setenteredAmount(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+       setenteredDate(event.target.value);
 }
 return <form>
     <div>
         <div>
         <label>Expense Title</label>
-        <input type= "text" onChange='titleChangeHandler'/>
+        <input type= "text" onChange={titleChangeHandler}/>
         </div>
         <div>
         <label>Expense amount</label>
-        <input type= "number"/>
+        <input type= "number" onChange={amountChangeHandler}/>
         </div>
         <div>
         <label>Expense date</label>
-        <input type= "date"/>
+        <input type= "date" onChange={dateChangeHandler}/>
         </div>
         <div>
             <button>Add Expenses</button>
